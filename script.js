6e6a94f9e73d5ace96f696fd710080a3f7dadb1e
jwhammond20 <jwhammond20@gmail.com>
2023-02-21 11:32:35 -0500

    updated css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Fetch the data from the API
 fetch("https://ranks.dynastynerds.com/top10")
   .then((response) => response.json())
   .then((data) => {
+    // Map the retrieved data to a simpler format
     const players = data.map((player) => {
       return {
         id: player.id,
@@ -14,19 +16,24 @@ fetch("https://ranks.dynastynerds.com/top10")
       };
     });
 
+    // Get the players table and its body element
     const playersTable = document.getElementById("players-table");
     const playersTableBody = playersTable.getElementsByTagName("tbody")[0];
 
+    // Iterate through the players array and add each player to the table
     players.forEach((player) => {
+      // Create a new row and cells for each player
       const row = playersTableBody.insertRow();
       const rankCell = row.insertCell();
       const playerCell = row.insertCell();
-      const teamCell = row.insertCell();
-      const positionCell = row.insertCell();
+      const rankPositionCell = row.insertCell();
       const positionalRankCell = row.insertCell();
 
+      // Set the text and styles for the rank cell
       rankCell.innerText = player.rank;
+      rankCell.classList.add("rank-cell");
 
+      // Set the text and styles for the player cell, which includes the image and name
       playerCell.classList.add("player-info");
       const image = document.createElement("img");
       image.src = player.image;
@@ -37,9 +44,17 @@ fetch("https://ranks.dynastynerds.com/top10")
       name.innerText = `${player.first_name_simple} ${player.last_name_simple}`;
       name.classList.add("player-name");
       playerCell.appendChild(name);
-      teamCell.innerText = player.team;
-      positionCell.innerText = player.position;
-      positionalRankCell.innerText = player.positional_rank;
+
+      // Set the text and styles for the rank/position cell
+      const rankPositionSpan = document.createElement("span");
+      rankPositionSpan.innerText = `${player.team} - ${player.position}`;
+      rankPositionSpan.classList.add("team-position-cell");
+      rankPositionSpan.classList.add(player.position);
+      rankPositionCell.appendChild(rankPositionSpan);
+
+      // Set the text and styles for the positional rank cell
+      positionalRankCell.innerText = `${player.position}-${player.positional_rank}`;
+      positionalRankCell.classList.add("pos-rank");
     });
   })
   .catch((error) => console.log(error));
